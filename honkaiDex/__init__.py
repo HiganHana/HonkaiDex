583dd5a8f0973be59b7a05eb0b03fb97c4a022cf
Celtica3 <celticaxp@gmail.com>
2022-05-26 11:55:27 -0700

    ref: remove all old

--- honkaiDex/__init__.py
+++
@@ -1,4 +0,0 @@
-from honkaiDex.base.stigamata import StigamataSet
-from honkaiDex.config import config, gconfig
-from honkaiDex.base.character import Battlesuit, BaseCharacter
-import honkaiDex.game as gamelogic
